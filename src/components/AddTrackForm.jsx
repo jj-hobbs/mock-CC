e9d46f05e5f6e6b736aa847aaf0437dfdaa8e5cd
justin.hobbs <justinhobbs@fastmail.com>
2022-06-16 14:08:30 -0500

    working cc!

--- src/components/AddTrackForm.jsx
+++ src/components/AddTrackForm.jsx
@@ -1,13 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function AddTrackForm() {
+function AddTrackForm({ onAddTrack }) {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [artist, setArtist] = useState("");
+  const [bpm, setBpm] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://localhost:8001/tracks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "image": image,
+        "title": title,
+        "artist": artist,
+        "BPM": bpm,
+      })
+    })
+    .then(resp => resp.json())
+    .then(newTrack => onAddTrack(newTrack));
+  }
+  
   return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <input value="" type="text" name="image" placeholder="Image URL"/>
-          <input value="" type="text" name="title" placeholder="title" />
-          <input value="" type="text" name="artist" placeholder="Artist" />
-          <input value="" type="number" name="bpm" placeholder="BPM" step="1.00" />
+          <input 
+            value="" 
+            type="text" 
+            name="image" 
+            placeholder="Image URL" 
+            // eslint-disable-next-line
+            value={image} 
+            onChange={(e) => setImage(e.target.value)}
+          />
+          <input 
+            value="" 
+            type="text" 
+            name="title" 
+            placeholder="title" 
+            // eslint-disable-next-line
+            value={title} 
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input 
+            value="" 
+            type="text" 
+            name="artist" 
+            placeholder="Artist" 
+            // eslint-disable-next-line
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+          />
+          <input 
+            value="" 
+            type="number" 
+            name="bpm" 
+            placeholder="BPM" 
+            step="1.00" 
+            // eslint-disable-next-line
+            value={bpm}
+            onChange={(e) => setBpm(e.target.value)}
+          />
         </div>
         <input className="" type="submit" value="Add Track" />
       </form>
--- src/components/TracksPage.jsx
+++ src/components/TracksPage.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './Search'
 import AddTrackForm from './AddTrackForm'
 import TracksList from './TracksList'
 
 function TracksPage() {
+  const [tracks, setTracks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    
+
+  useEffect(() => {
+    fetch("http://localhost:8001/tracks")
+    .then((resp) => resp.json())
+    .then((tracksArray) => {
+        setTracks(tracksArray);
+    })
+}, []);  
+
+function handleAddTrack(newTrack) {
+  const updatedTracksArray = [...tracks, newTrack]
+  setTracks(updatedTracksArray)
+}
+
+const displayedTracks = tracks.filter((track) => {
+  return track.title.toLowerCase().includes(searchTerm.toLowerCase());
+});
+
   return (
     <div>
-      <Search />
-      <AddTrackForm />
-      <TracksList />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
+      <AddTrackForm onAddTrack={handleAddTrack}/>
+      <TracksList tracks={displayedTracks}/>
     </div>
   )
 }
